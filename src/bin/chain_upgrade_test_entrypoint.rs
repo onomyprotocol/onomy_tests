31257fb0ed524098d80d01f6121849d2c7329efd
Aaron Kutch <aaronkutch@att.net>
2023-05-12 01:52:30 -0500

    great improvements

--- src/bin/chain_upgrade_test_entrypoint.rs
+++ src/bin/chain_upgrade_test_entrypoint.rs
@@ -1,9 +1,9 @@
 use std::env;
 
-use common::{get_separated_val, nom};
+use common::nom;
 use lazy_static::lazy_static;
 use serde_json::{json, Value};
-use super_orchestrator::{acquire_file_path, sh, Command, MapAddError, Result};
+use super_orchestrator::{acquire_file_path, get_separated_val, sh, Command, MapAddError, Result};
 use tokio::{fs::OpenOptions, io::AsyncReadExt};
 
 lazy_static! {
@@ -23,9 +23,9 @@ async fn main() -> Result<()> {
     // NOTE: this is stuff you would not want to run in production
 
     let chain_id = "onomy";
-    sh("cosmovisor", &["run", "config", "chain-id", chain_id]).await?;
-    sh("cosmovisor", &["run", "config", "keyring-backend", "test"]).await?;
-    sh("cosmovisor", &["run", "init", chain_id, "--overwrite"]).await?;
+    sh("cosmovisor run config chain-id", &[chain_id]).await?;
+    sh("cosmovisor run config keyring-backend test", &[]).await?;
+    sh("cosmovisor run init --overwrite", &[chain_id]).await?;
 
     let genesis_file_path =
         acquire_file_path(&format!("{}/config/genesis.json", *DAEMON_HOME)).await?;
@@ -49,26 +49,23 @@ async fn main() -> Result<()> {
     genesis["app_state"]["gov"]["voting_params"]["voting_period"] = gov_period.clone();
     genesis["app_state"]["gov"]["deposit_params"]["max_deposit_period"] = gov_period;
 
-    sh("cosmovisor", &["run", "keys", "add", "validator"]).await?;
-    sh("cosmovisor", &[
-        "run",
-        "add-genesis-account",
-        "validator",
-        &nom(2.0e6),
-    ])
+    sh("cosmovisor run keys add validator", &[]).await?;
+    sh("cosmovisor run add-genesis-account validator", &[&nom(
+        2.0e6,
+    )])
     .await?;
     // Even if we don't test the bridge, we need this because SetValsetRequest is
     // called by the gravity module. There are parallel validators for the
     // gravity module, and they need all their own `gravity` variations of `gentx`
     // and `collect-gentxs`
-    sh("cosmovisor", &["run", "keys", "add", "orchestrator"]).await?;
-    let eth_keys = Command::new("cosmovisor", &["run", "eth_keys", "add"])
+    sh("cosmovisor run keys add orchestrator", &[]).await?;
+    let eth_keys = Command::new("cosmovisor run eth_keys add", &[])
         .run_to_completion()
         .await?
         .stdout;
     let eth_addr = get_separated_val(&eth_keys, "\n", "address", ":")?;
     // skip the first "INF" line
-    let orch_addr = Command::new("cosmovisor", &["run", "keys", "show", "orchestrator", "-a"])
+    let orch_addr = Command::new("cosmovisor run keys show orchestrator -a", &[])
         .run_to_completion()
         .await?
         .stdout
@@ -76,15 +73,12 @@ async fn main() -> Result<()> {
         .nth(1)
         .map_add_err(|| ())?
         .to_string();
-    sh("cosmovisor", &[
-        "run",
-        "add-genesis-account",
-        "orchestrator",
-        &nom(2.0e6),
-    ])
+    sh("cosmovisor run add-genesis-account orchestrator", &[&nom(
+        2.0e6,
+    )])
     .await?;
 
-    sh("cosmovisor", &["run", "gravity", "gentx", "validator"]).await?;
+    //sh("cosmovisor", &["run", "gravity", "gentx", "validator"]).await?;
 
     Ok(())
 }
