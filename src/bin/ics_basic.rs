14431efd3e843b394b071523541449bc32805b04
Aaron Kutch <aaronkutch@att.net>
2023-05-29 14:46:45 -0500

    Update ics_basic.rs

--- src/bin/ics_basic.rs
+++ src/bin/ics_basic.rs
@@ -55,21 +55,20 @@ async fn container_runner() -> Result<()> {
     // FIXME fall back to 971000347e9dce20e27b37208a0305c27ef1c458
 
     // build binaries
-    /*
-        sh("make --directory ./../onomy_workspace0/onomy/ build", &[]).await?;
-        sh("make --directory ./../market/ build", &[]).await?;
-        // copy to dockerfile resources (docker cannot use files from outside cwd)
-        sh(
-            "cp ./../onomy_workspace0/onomy/onomyd ./dockerfiles/dockerfile_resources/onomyd",
-            &[],
-        )
-        .await?;
-        sh(
-            "cp ./../market/marketd ./dockerfiles/dockerfile_resources/marketd",
-            &[],
-        )
-        .await?;
-    */
+
+    sh("make --directory ./../onomy_workspace0/onomy/ build", &[]).await?;
+    sh("make --directory ./../market/ build", &[]).await?;
+    // copy to dockerfile resources (docker cannot use files from outside cwd)
+    sh(
+        "cp ./../onomy_workspace0/onomy/onomyd ./dockerfiles/dockerfile_resources/onomyd",
+        &[],
+    )
+    .await?;
+    sh(
+        "cp ./../market/marketd ./dockerfiles/dockerfile_resources/marketd",
+        &[],
+    )
+    .await?;
 
     // prepare volumed resources
     remove_files_in_dir("./resources/keyring-test/", &["address", "info"]).await?;
@@ -122,188 +121,31 @@ async fn container_runner() -> Result<()> {
     Ok(())
 }
 
-async fn onomyd_runner() -> Result<()> {
-    //let hostname = "onomyd";
-    let consumer_hostname = "marketd:26000";
-    let mut nm = NetMessenger::connect(STD_TRIES, STD_DELAY, consumer_hostname)
-        .await
-        .map_add_err(|| ())?;
-
-    let mut nm_hermes = NetMessenger::connect(STD_TRIES, STD_DELAY, "hermes:26000")
-        .await
-        .map_add_err(|| ())?;
-
-    let gov_period = "4s";
-    let daemon_home = DAEMON_HOME.as_str();
-    let mnemonic = onomyd_setup(daemon_home, gov_period).await?;
-
-    let mut cosmovisor_runner = cosmovisor_start("onomyd_runner.log", true, None).await?;
-
-    let proposal_id = "1";
-
-    // `json!` doesn't like large literals beyond i32
-    let proposal_s = r#"{
-        "title": "Propose the addition of a new chain",
-        "description": "add consumer chain market",
-        "chain_id": "market",
-        "initial_height": {
-            "revision_number": 0,
-            "revision_height": 1
-        },
-        "genesis_hash": "Z2VuX2hhc2g=",
-        "binary_hash": "YmluX2hhc2g=",
-        "spawn_time": "2023-05-18T01:15:49.83019476-05:00",
-        "consumer_redistribution_fraction": "0.75",
-        "blocks_per_distribution_transmission": 1000,
-        "historical_entries": 10000,
-        "ccv_timeout_period": 2419200000000000,
-        "transfer_timeout_period": 3600000000000,
-        "unbonding_period": 1728000000000000,
-        "deposit": "2000000000000000000000anom"
-    }"#;
-    // we will just place the file under the config folder
-    let proposal_file_path = format!("{daemon_home}/config/consumer_add_proposal.json");
-    FileOptions::write_str(&proposal_file_path, proposal_s)
-        .await
-        .map_add_err(|| ())?;
-
-    let gas_args = [
-        "--gas",
-        "auto",
-        "--gas-adjustment",
-        "1.3",
-        "-y",
-        "-b",
-        "block",
-        "--from",
-        "validator",
-    ]
-    .as_slice();
-    cosmovisor(
-        "tx gov submit-proposal consumer-addition",
-        &[&[proposal_file_path.as_str()], gas_args].concat(),
-    )
-    .await?;
-    // the deposit is done as part of the chain addition proposal
-    cosmovisor(
-        "tx gov vote",
-        &[[proposal_id, "yes"].as_slice(), gas_args].concat(),
-    )
-    .await?;
-
-    // In the mean time get hermes keys and consensus key assignment done
-
-    nm_hermes.send::<String>(&mnemonic).await?;
-
-    // FIXME this should be from $DAEMON_HOME/config/priv_validator_key.json, not
-    // some random thing from teh validator set
-    let tmp_s = get_separated_val(
-        &cosmovisor("query tendermint-validator-set", &[]).await?,
-        "\n",
-        "value",
-        ":",
-    )?;
-    let mut consensus_pubkey = r#"{"@type":"/cosmos.crypto.ed25519.PubKey","key":""#.to_owned();
-    consensus_pubkey.push_str(&tmp_s);
-    consensus_pubkey.push_str("\"}}");
-
-    info!("ccvkey: {consensus_pubkey}");
-
-    // do this before getting the consumer-genesis
-    cosmovisor(
-        "tx provider assign-consensus-key market",
-        &[[consensus_pubkey.as_str()].as_slice(), gas_args].concat(),
-    )
-    .await?;
-
-    wait_for_height(STD_TRIES, STD_DELAY, 5).await?;
-
-    let ccvconsumer_state =
-        cosmovisor("query provider consumer-genesis market -o json", &[]).await?;
-
-    info!("ccvconsumer_state:\n{ccvconsumer_state}\n\n");
-
-    // send to `marketd`
-    nm.send::<String>(&ccvconsumer_state).await?;
-
-    let genesis_s =
-        FileOptions::read_to_string(&format!("{daemon_home}/config/genesis.json")).await?;
-    info!("genesis: {genesis_s}");
-    let genesis: Value = serde_json::from_str(&genesis_s)?;
-    nm.send::<String>(&genesis["app_state"]["auth"]["accounts"].to_string())
-        .await?;
-    nm.send::<String>(&genesis["app_state"]["bank"].to_string())
-        .await?;
-    nm.send::<String>(
-        &FileOptions::read_to_string(&format!("{daemon_home}/config/node_key.json")).await?,
-    )
-    .await?;
-    nm.send::<String>(
-        &FileOptions::read_to_string(&format!("{daemon_home}/config/priv_validator_key.json"))
-            .await?,
-    )
-    .await?;
-
-    // wait for marketd to be online
-    nm.recv::<()>().await?;
-    nm_hermes.send::<()>(&()).await?;
-
-    sleep(TIMEOUT).await;
-    cosmovisor_runner.terminate().await?;
-    Ok(())
-}
-
-async fn marketd_runner() -> Result<()> {
+async fn hermes_runner() -> Result<()> {
     let listen = "0.0.0.0:26000";
     let mut nm = NetMessenger::listen_single_connect(listen, TIMEOUT).await?;
 
-    let daemon_home = DAEMON_HOME.as_str();
-    let chain_id = "market";
-    cosmovisor("config chain-id", &[chain_id]).await?;
-    cosmovisor("config keyring-backend test", &[]).await?;
-    cosmovisor("init --overwrite", &[chain_id]).await?;
-    let genesis_file_path = format!("{daemon_home}/config/genesis.json");
-
-    let ccvconsumer_state_s: String = nm.recv().await?;
-    let ccvconsumer_state: Value = serde_json::from_str(&ccvconsumer_state_s)?;
-
-    let accounts_s: String = nm.recv().await?;
-    let accounts: Value = serde_json::from_str(&accounts_s)?;
-
-    let bank_s: String = nm.recv().await?;
-    let bank: Value = serde_json::from_str(&bank_s)?;
-
-    // add `ccvconsumer_state` to genesis
-
-    let genesis_s = FileOptions::read_to_string(&genesis_file_path).await?;
-
-    let mut genesis: Value = serde_json::from_str(&genesis_s)?;
-    genesis["app_state"]["ccvconsumer"] = ccvconsumer_state;
-    // TODO see if we can remove these
-    genesis["app_state"]["auth"]["accounts"] = accounts;
-    genesis["app_state"]["bank"] = bank;
-    let genesis_s = genesis.to_string();
-    let genesis_s = genesis_s.replace("\"stake\"", "\"anom\"");
-
-    info!("genesis: {genesis_s}");
-
-    FileOptions::write_str(&genesis_file_path, &genesis_s).await?;
-
-    FileOptions::write_str(
-        &format!("{daemon_home}/config/node_key.json"),
-        &nm.recv::<String>().await?,
+    let mnemonic: String = nm.recv().await?;
+    FileOptions::write_str("/root/.hermes/mnemonic.txt", &mnemonic).await?;
+    sh(
+        "hermes keys add --chain onomy --mnemonic-file /root/.hermes/mnemonic.txt",
+        &[],
     )
     .await?;
-    FileOptions::write_str(
-        &format!("{daemon_home}/config/priv_validator_key.json"),
-        &nm.recv::<String>().await?,
+    sh(
+        "hermes keys add --chain market --mnemonic-file /root/.hermes/mnemonic.txt",
+        &[],
     )
     .await?;
 
-    let mut cosmovisor_runner = cosmovisor_start("marketd_runner.log", true, None).await?;
-
-    // signal that we have started
-    nm.send::<()>(&()).await?;
+    nm.recv::<()>().await?;
+    sh(
+        "hermes create connection --a-chain market --a-client 07-tendermint-0 --b-client \
+         07-tendermint-0",
+        &[],
+    )
+    .await?;
+    info!("\n\n\nREADY\n\n\n");
 
     // need
     // $DAEMON_HOME/data/priv_validator_state.json # good
@@ -383,36 +225,194 @@ async fn marketd_runner() -> Result<()> {
     // connection-0 --dst-port provider --src-port consumer --src-channel channel-0
 
     sleep(TIMEOUT).await;
+    Ok(())
+}
+
+async fn onomyd_runner() -> Result<()> {
+    let mut nm_hermes = NetMessenger::connect(STD_TRIES, STD_DELAY, "hermes:26000")
+        .await
+        .map_add_err(|| ())?;
+    let mut nm_marketd = NetMessenger::connect(STD_TRIES, STD_DELAY, "marketd:26000")
+        .await
+        .map_add_err(|| ())?;
+
+    let gov_period = "4s";
+    let daemon_home = DAEMON_HOME.as_str();
+    let mnemonic = onomyd_setup(daemon_home, gov_period).await?;
+
+    let mut cosmovisor_runner = cosmovisor_start("onomyd_runner.log", true, None).await?;
+
+    let proposal_id = "1";
+
+    // `json!` doesn't like large literals beyond i32
+    let proposal_s = r#"{
+        "title": "Propose the addition of a new chain",
+        "description": "add consumer chain market",
+        "chain_id": "market",
+        "initial_height": {
+            "revision_number": 0,
+            "revision_height": 1
+        },
+        "genesis_hash": "Z2VuX2hhc2g=",
+        "binary_hash": "YmluX2hhc2g=",
+        "spawn_time": "2023-05-18T01:15:49.83019476-05:00",
+        "consumer_redistribution_fraction": "0.75",
+        "blocks_per_distribution_transmission": 1000,
+        "historical_entries": 10000,
+        "ccv_timeout_period": 2419200000000000,
+        "transfer_timeout_period": 3600000000000,
+        "unbonding_period": 1728000000000000,
+        "deposit": "2000000000000000000000anom"
+    }"#;
+    // we will just place the file under the config folder
+    let proposal_file_path = format!("{daemon_home}/config/consumer_add_proposal.json");
+    FileOptions::write_str(&proposal_file_path, proposal_s)
+        .await
+        .map_add_err(|| ())?;
+
+    let gas_args = [
+        "--gas",
+        "auto",
+        "--gas-adjustment",
+        "1.3",
+        "-y",
+        "-b",
+        "block",
+        "--from",
+        "validator",
+    ]
+    .as_slice();
+    cosmovisor(
+        "tx gov submit-proposal consumer-addition",
+        &[&[proposal_file_path.as_str()], gas_args].concat(),
+    )
+    .await?;
+    // the deposit is done as part of the chain addition proposal
+    cosmovisor(
+        "tx gov vote",
+        &[[proposal_id, "yes"].as_slice(), gas_args].concat(),
+    )
+    .await?;
+
+    // In the mean time get consensus key assignment done
+
+    // FIXME this should be from $DAEMON_HOME/config/priv_validator_key.json, not
+    // some random thing from teh validator set
+    let tmp_s = get_separated_val(
+        &cosmovisor("query tendermint-validator-set", &[]).await?,
+        "\n",
+        "value",
+        ":",
+    )?;
+    let mut consensus_pubkey = r#"{"@type":"/cosmos.crypto.ed25519.PubKey","key":""#.to_owned();
+    consensus_pubkey.push_str(&tmp_s);
+    consensus_pubkey.push_str("\"}}");
+
+    info!("ccvkey: {consensus_pubkey}");
+
+    // do this before getting the consumer-genesis
+    cosmovisor(
+        "tx provider assign-consensus-key market",
+        &[[consensus_pubkey.as_str()].as_slice(), gas_args].concat(),
+    )
+    .await?;
+
+    wait_for_height(STD_TRIES, STD_DELAY, 5).await?;
+
+    let ccvconsumer_state =
+        cosmovisor("query provider consumer-genesis market -o json", &[]).await?;
+
+    info!("ccvconsumer_state:\n{ccvconsumer_state}\n\n");
+
+    nm_hermes.send::<String>(&mnemonic).await?;
+
+    // send to `marketd`
+    nm_marketd.send::<String>(&ccvconsumer_state).await?;
+
+    let genesis_s =
+        FileOptions::read_to_string(&format!("{daemon_home}/config/genesis.json")).await?;
+    info!("genesis: {genesis_s}");
+    let genesis: Value = serde_json::from_str(&genesis_s)?;
+    nm_marketd
+        .send::<String>(&genesis["app_state"]["auth"]["accounts"].to_string())
+        .await?;
+    nm_marketd
+        .send::<String>(&genesis["app_state"]["bank"].to_string())
+        .await?;
+    nm_marketd
+        .send::<String>(
+            &FileOptions::read_to_string(&format!("{daemon_home}/config/node_key.json")).await?,
+        )
+        .await?;
+    nm_marketd
+        .send::<String>(
+            &FileOptions::read_to_string(&format!("{daemon_home}/config/priv_validator_key.json"))
+                .await?,
+        )
+        .await?;
+
+    // wait for marketd to be online
+    nm_marketd.recv::<()>().await?;
+    nm_hermes.send::<()>(&()).await?;
+
+    sleep(TIMEOUT).await;
     cosmovisor_runner.terminate().await?;
     Ok(())
 }
 
-async fn hermes_runner() -> Result<()> {
+async fn marketd_runner() -> Result<()> {
     let listen = "0.0.0.0:26000";
     let mut nm = NetMessenger::listen_single_connect(listen, TIMEOUT).await?;
 
-    let mnemonic: String = nm.recv().await?;
-    FileOptions::write_str("/root/.hermes/mnemonic.txt", &mnemonic).await?;
-    sh(
-        "hermes keys add --chain onomy --mnemonic-file /root/.hermes/mnemonic.txt",
-        &[],
+    let daemon_home = DAEMON_HOME.as_str();
+    let chain_id = "market";
+    cosmovisor("config chain-id", &[chain_id]).await?;
+    cosmovisor("config keyring-backend test", &[]).await?;
+    cosmovisor("init --overwrite", &[chain_id]).await?;
+    let genesis_file_path = format!("{daemon_home}/config/genesis.json");
+
+    let ccvconsumer_state_s: String = nm.recv().await?;
+    let ccvconsumer_state: Value = serde_json::from_str(&ccvconsumer_state_s)?;
+
+    let accounts_s: String = nm.recv().await?;
+    let accounts: Value = serde_json::from_str(&accounts_s)?;
+
+    let bank_s: String = nm.recv().await?;
+    let bank: Value = serde_json::from_str(&bank_s)?;
+
+    // add `ccvconsumer_state` to genesis
+
+    let genesis_s = FileOptions::read_to_string(&genesis_file_path).await?;
+
+    let mut genesis: Value = serde_json::from_str(&genesis_s)?;
+    genesis["app_state"]["ccvconsumer"] = ccvconsumer_state;
+    // TODO see if we can remove these
+    genesis["app_state"]["auth"]["accounts"] = accounts;
+    genesis["app_state"]["bank"] = bank;
+    let genesis_s = genesis.to_string();
+    let genesis_s = genesis_s.replace("\"stake\"", "\"anom\"");
+
+    info!("genesis: {genesis_s}");
+
+    FileOptions::write_str(&genesis_file_path, &genesis_s).await?;
+
+    FileOptions::write_str(
+        &format!("{daemon_home}/config/node_key.json"),
+        &nm.recv::<String>().await?,
     )
     .await?;
-    sh(
-        "hermes keys add --chain market --mnemonic-file /root/.hermes/mnemonic.txt",
-        &[],
+    FileOptions::write_str(
+        &format!("{daemon_home}/config/priv_validator_key.json"),
+        &nm.recv::<String>().await?,
     )
     .await?;
 
-    nm.recv::<()>().await?;
-    sh(
-        "hermes create connection --a-chain market --a-client 07-tendermint-0 --b-client \
-         07-tendermint-0",
-        &[],
-    )
-    .await?;
-    info!("\n\n\nREADY\n\n\n");
+    let mut cosmovisor_runner = cosmovisor_start("marketd_runner.log", true, None).await?;
+
+    // signal that we have started
+    nm.send::<()>(&()).await?;
 
     sleep(TIMEOUT).await;
+    cosmovisor_runner.terminate().await?;
     Ok(())
 }
